6145b765b8c984c98d51289c4cbefeb24b092fe3
Yoliber Pichardo <yoliberfelixpp01@gmail.com>
2022-07-06 09:55:41 -0400

    layout created

---
+++ src/layouts/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bodynav">
+        <router-link to="/">Home</router-link>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="home">
+    <NavBar />
     <User />
   </div>
 </template>
 
 <script>
 import User from '../components/User.vue'
+import NavBar from '../layouts/NavBar.vue'
 
 export default {
   name: 'Home',
   components: {
-    User
+    User,
+    NavBar
 }
 }
 </script>
